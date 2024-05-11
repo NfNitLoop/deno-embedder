dba25b791dfe9fef12fc5329532f414d2fba80a5
Cody Casterline <cody.casterline@gmail.com>
2024-05-10 22:00:56 -0700

    Fix Deno Publish error.

--- deps/std/media_types.ts
+++ deps/std/media_types.ts
@@ -1,6 +1,6 @@
-export * from "jsr:@std/media-types"
+export * from "jsr:@std/media-types@0.224.0"
 
-import { typeByExtension } from "jsr:@std/media-types"
+import { typeByExtension } from "jsr:@std/media-types@0.224.0"
 import { extname } from "./path.ts"
 
 // Backward compatability for old deno.land/x/media_types:
